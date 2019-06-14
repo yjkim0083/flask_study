6d683eb23b729a33eaf0af12218e9bb0530fab83
yjkim0083 <yjkim0083@gmail.com>
2019-06-14 16:10:32 +0900

    flask api 완성

--- app222.py
+++ app222.py
@@ -1,35 +1,33 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import json
-
-import numpy as np
 import csv
+import numpy as np
+import time
 
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route('/')
 def hello_world():
-    # csvParser()
-    # #return 'Hello World!'
-    # data = {
-    #     "name":123,
-    #     "age":444,
-    #     "grade":1
-    # }
-    #return data
-    #return Flask.Response(json.dumps(data), mimetype='application/json')
-    return jsonify(result=csvParser())
+    print(time.time())
+    x, y, label = csvParser()
+    print(time.time())
+    return jsonify(x=x, y=y, label=label)
 
 def csvParser():
-    data_path="hello.csv"
+    data_path="tSNE-example8.csv"
     with open(data_path, 'r') as f:
         reader = csv.reader(f, delimiter=",")
+        header = next(reader)
         data = list(reader)
-        # _data = np.array(data).astype(float)
-    return list(data)
 
+    # convert list to npdarray
+    _data = np.array(data)
+    x = _data[:, 1:2].flatten().tolist()
+    y = _data[:, 2:3].flatten().tolist()
+    label = _data[:, -1:].flatten().tolist()
+
+    return x, y, label
 
 if __name__ == '__main__':
     app.run()
